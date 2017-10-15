08e600e68e5754dd4a50f59b3520e02f0e433199
Thiefy <lil@thiefy.one>
2017-10-15 11:44:34 -0400

    simple cmline disasm added

---
+++ simple_disasm.py
@@ -0,0 +1,25 @@
+from capstone import *
+import sys
+
+def main():
+    if "x" in sys.argv[1]:
+        try:
+            shellcode = ''.join([chr(int(i,16)) for i in filter(None,sys.argv[1].split("x"))])
+        except Exception as e:
+            print e
+            print "[x.x] Usage: %s <shellcode> (e.g. \\x11\\x22\\x33...)"
+            sys.exit() 
+    else:
+        shellcode = ""
+        for i in range(0,len(sys.argv[1]),2):
+            try:
+                shellcode+=chr(int(sys.argv[1][i:i+2],16))
+            except TypeError:
+                pass
+
+    md = Cs(CS_ARCH_X86,CS_MODE_64)
+    for i in md.disasm(shellcode,0x0):
+        print "0x%x | %s %s"%(i.address,i.mnemonic,i.op_str)
+   
+if __name__ == "__main__":
+    main()
