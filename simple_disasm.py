04efc46ff800fdf8911941898616b186bfb5aa50
Thiefy <lil@thiefy.one>
2017-10-15 12:11:52 -0400

    Slightly prettier

--- simple_disasm.py
+++ simple_disasm.py
@@ -3,9 +3,10 @@ from capstone import *
 import sys
 
 def main():
+    print "[^_^] Simple Disassembler"
     if "x" in sys.argv[1]:
         try:
-            shellcode = ''.join([chr(int(i,16)) for i in filter(None,sys.argv[1].split("x"))])
+            shellcode = ''.join([chr(int(i,16)) for i in filter(None,sys.argv[1].split("\\x"))])
         except Exception as e:
             print e
             print "[x.x] Usage: %s <shellcode> (e.g. \\x11\\x22\\x33...)"
@@ -18,10 +19,17 @@ def main():
             except TypeError:
                 pass
 
+    buf = ""
     md = Cs(CS_ARCH_X86,CS_MODE_64)
     for i in md.disasm(shellcode,0x0):
+        line = ""
         b = "\\x"+"\\x".join(["%02x"%x for x in i.bytes])
-        print "0x%x |  %s %s %s"%(i.address,i.mnemonic,i.op_str,b)
+        line += "0x%04x |  %s %s"%(i.address,i.mnemonic,i.op_str)
+        line += " "*(40-len(line))
+        line += b
+        line += "\n"
+        buf += line
+    print buf
    
 if __name__ == "__main__":
     main()
